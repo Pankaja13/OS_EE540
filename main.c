f19995c2a6bf1f3fdbed2e416ba1b8ca9c0375c8
Pankaja W <pankaja13.psd@gmail.com>
2019-02-16 14:41:45 -0600

    single array input

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ARRAY 10
 
 
 int main() {
-	printf("Hello!\n");
+	int number;
+	printf("Enter Number: ");
+	scanf("%i", &number);
+	printf("%i\n", number);
+	if(number > MAX_ARRAY) {
+		exit(-1);
+	}
+
+	int array[MAX_ARRAY] = {0};
+
+	printf("Type ya numbers(%i): ",number);
+	for (int i = 0; i < number; ++i) {
+		int input = 0;
+		scanf("%i", &input);
+		array[i] = input;
+		printf(" ");
+	}
+
+	printf("Output: ");
+	for (int j = 0; j < 9; ++j) {
+		printf("%i ",array[j]);
+	}
 }

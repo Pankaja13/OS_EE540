aa77008314c0036adf79445b4dcf78679fbe5d56
Pankaja W <pankaja13.psd@gmail.com>
2019-02-24 13:27:57 -0600

    add matrix multiply logic

--- main.c
+++ main.c
@@ -9,10 +9,12 @@ struct Matrix {
 	int y;
 	int data[MAX_ARRAY][MAX_ARRAY];
 };
+struct Matrix A;
+struct Matrix B;
 
 void printArray(struct Matrix thisMatrix);
 
-int main() {
+struct Matrix getArrayFromInput(){
 	int x;
 	int y;
 
@@ -36,7 +38,65 @@ int main() {
 		}
 		fflush(stdin);
 	}
-	printArray(temp);
+	return temp;
+}
+
+int getMatrixProduct(int col, int row){
+//	printf("Output Array Size: %i\n", A.x*B.y);
+	if (A.y != B.x) {
+		printf("Size Mismatch: %i %i\n", A.y, B.x);
+		return -1;
+	}
+//
+//	for (int l = 0; l <A.x; ++l) {
+//		for (int k = 0; k < B.y; ++k) {
+//			printf("%i %i\n",l, k);
+//		}
+//	}
+
+	int rowValues[10] = {0};
+	for (int k = 0; k < A.x; ++k) {
+		int val = A.data[k][col - 1];
+//		printf("%i ", val);
+		rowValues[k] = val;
+	}
+
+	printf("\n");
+
+	int colValues[10] = {0};
+	for (int l = 0; l < B.y; ++l) {
+		int val = B.data[row - 1][l];
+//		printf("%i\n", val);
+		colValues[l] = val;
+	}
+
+	int sum = 0;
+	for (int i = 0; i < 10; ++i) {
+		sum = sum + (rowValues[i] * colValues[i]);
+	}
+	printf("SUM: %i\n", sum);
+
+	return 1;
+}
+
+int main() {
+	A = getArrayFromInput();
+	B = getArrayFromInput();
+
+	printArray(A);
+	printArray(B);
+
+	printf("---------------\n");
+	for (int i = 1; i < A.x; ++i) {
+		for (int j = 1; j < B.y; ++j) {
+			getMatrixProduct(i,j);
+//			printf("%i %i\n",i, j);
+		}
+	}
+
+	printf("---------------\n");
+
+
 }
 
 void printArray(struct Matrix thisMatrix) {
